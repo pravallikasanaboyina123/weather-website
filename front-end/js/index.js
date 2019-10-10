c6ff11e46293f36404b4212e6ab08f92c811f6fb
khushi kumari <khushi.kumari@credencys.com>
2019-10-10 12:32:20 +0530

    remove unnecessary console.log call

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,4 +1,4 @@
-console.log('client side js file.')
+
 //getting puzzle website as a reference
 // fetch('http://puzzle.mead.io/puzzle').then((response) => {
 //     response.json().then((data) => {
