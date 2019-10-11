004f8c7fc782d7d5951b67a8ed9f677bfce9675e
sanaboyina pravallika <pravallika.sanaboyina@credencys.com>
2019-10-11 14:21:09 +0530

    high low temp added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast=(latitude,longitude,callback)=>{
    }else if(body.error){
      callback('Unable to find location.',undefined)
    }else{
-  callback(undefined,body.daily.data[0].summary+ ' It is currently '+body.currently.temperature+' degrees out. There is a '+body.currently.precipProbability+'% chance of rain.') 
+  callback(undefined,body.daily.data[0].summary+ ' It is currently '+body.currently.temperature+' degrees out. This high today is '+ body.daily.data[0].temparauteHigh +' with low  of '+ body.daily.data[0].temparauteLow +' There is a '+body.currently.precipProbability+'% chance of rain.') 
 }
 })
 }
